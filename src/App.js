8147d6d117dc0e105708f5871511d01952ba18a4
Samuel <aritasam98@gmail.com>
2023-08-14 19:05:05 +0300

    feat: Commented out react-router-dom script

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import {Route, Routes} from 'react-router-dom';
+// import {Route, Routes} from 'react-router-dom';
 import Body from './components/Body/body';
 
 
