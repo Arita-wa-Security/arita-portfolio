b42535b0a3192f49289acdcf2c9d0e0b6179e529
Samuel <aritasam98@gmail.com>
2023-09-18 19:46:07 +0300

    style: Styled the body section

--- src/components/Body/body.js
+++ src/components/Body/body.js
@@ -7,33 +7,30 @@ import Stack from "./Stack/Stack";
 
 function Body(){
  return (
-  <>
-   <div class="d-flex justify-content-evenly" id="Body">
-     <div id="initial-content">
-       
-       <h5>“I learned that courage was not the absence of fear, 
-        <br></br>but the triumph over it. 
-        <br></br>The brave man is not he who does not feel afraid,
-        <br></br>but he who conquers that fear.” 
-        <br></br>
-        <br></br>Nelson Mandela
-        </h5>
-     </div>
+   <>
+     <div class="d-flex justify-content-evenly" id="Body">
+       <div id="initial-content">
+         <h5>
+           “I learned that courage was not the absence of fear,
+           <br></br>but the triumph over it.
+           <br></br>The brave man is not he who does not feel afraid,
+           <br></br>but he who conquers that fear.”
+           <br></br>
+           <br></br>Nelson Mandela
+         </h5>
+       </div>
 
-     {/* <div id="work">
+       {/* <div id="work">
        <img
          alt="cyber-security"
          src={Work}
          id="work"
        />
      </div> */}
-     
-   </div>
-   <Resume />
-   <Projects />
-   <Stack />
-
-
+     </div>
+     <Resume />
+     <Projects />
+     <Stack />
    </>
  );
 }
